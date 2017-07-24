2ec20e1c745a81a660169cf1ed7955b552ffd3f5
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2017-07-24 10:45:58 +0300

    Add pagination to articles and sort them by recent on the main page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
-  expose_decorated :articles, -> { Article.includes(:user) }
+  expose_decorated :articles, -> { paginated_articles }
   expose_decorated :article
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def paginated_articles
+    Article.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
   end
 end
