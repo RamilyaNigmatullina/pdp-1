f3155905e95aa9acf0ca9ec64cf73f7669dcacef
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2017-07-27 11:09:36 +0300

    [#147419385] Add comments

---
+++ app/controllers/users/comments_controller.rb
@@ -0,0 +1,22 @@
+module Users
+  class CommentsController < Users::BaseController
+    expose(:article)
+    expose_decorated(:comments) { Comment.by_article(article) }
+    expose(:comment, parent: :article)
+
+    def create
+      self.comment = new_comment if comment.save
+      render "articles/fragments"
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+
+    def new_comment
+      article.comments.build
+    end
+  end
+end
