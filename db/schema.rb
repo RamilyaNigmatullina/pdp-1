b96decb98ddf36a81312e6c8b599354baef59152
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2017-08-12 19:33:10 +0300

    Add some styles and add user to comments

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715181507) do
+ActiveRecord::Schema.define(version: 20170812153348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +29,9 @@ ActiveRecord::Schema.define(version: 20170715181507) do
     t.integer  "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +58,5 @@ ActiveRecord::Schema.define(version: 20170715181507) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
