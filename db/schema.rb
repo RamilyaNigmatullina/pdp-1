21f3d12d992892a264cb398e97a5a5481639d473
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2017-08-24 10:46:54 +0300

    Correct schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,9 @@ ActiveRecord::Schema.define(version: 20170823083553) do
     t.integer  "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "ratings", force: :cascade do |t|
@@ -68,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170823083553) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
   add_foreign_key "ratings", "articles"
   add_foreign_key "ratings", "users"
 end
