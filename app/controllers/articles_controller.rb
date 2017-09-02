d2c83b492ad2bd3db8b4688a36a37d6944439322
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2017-09-02 06:50:21 +0300

    Add ransack gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  expose_decorated :articles, -> { paginated_articles }
+  expose_decorated :articles
   expose_decorated :article
 
   expose_decorated(:comments) { article.comments }
@@ -9,16 +9,14 @@ class ArticlesController < ApplicationController
   helper_method :current_user_rating
 
   def index
+    @q = Article.includes(:user).ransack(params[:q])
+    self.articles = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
   end
 
   private
-
-  def paginated_articles
-    Article.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
-  end
 
   def current_user_rating
     @rating ||= article.ratings.find_by(user: current_user)
